d5b277b00942fc92aba92690589270b6310d80e7
Joshua Jackson <washingtonfaulconer@gmail.com>
2018-05-07 12:57:37 +0100

    Initial commit of Coffee House Site

---
+++ Coffee-House/Gruntfile.js
@@ -0,0 +1,77 @@
+
+module.exports = function (grunt)
+{
+    grunt.initConfig(
+        {
+            sass:
+                {
+                    dev:
+                        {
+                            files:
+                                {
+                                    //Destination -> //Source File
+                                    'styles/main.css': 'styles/main.scss',
+                                }
+                        }
+                },
+
+            cssmin:
+                {
+                    build:
+                        {
+                            src: 'styles/main.css',
+                            dest: 'styles/main.min.css'
+                        }
+                },
+
+            uglify:
+                {
+                    build:
+                        {
+                            files:
+                                {
+                                    'scripts/main.min.js': ['scripts/main.js']
+                                }
+                        }
+                },
+
+            watch:
+                {
+                    sass:
+                        {
+                            files: '**/*.scss',
+                            tasks: ['css'],
+                            options:
+                                {
+                                    livereload: true
+                                }
+                        },
+                    uglify:
+                        {
+                            files: 'scripts/main.js',
+                            tasks: ['js'],
+                            options:
+                                {
+                                    livereload: true
+                                }
+                        },
+                    all: {
+                        files: ['**/*.html'],
+                        options: {
+                            livereload: true
+                        }
+                    }
+
+                }
+        });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('css', ['sass', 'cssmin']);
+    grunt.registerTask('js', ['uglify']);
+};
